0960dc5e9e5a2f9395b896abce553034d0cc4671
AyaanTheDev <splytplayz@gmail.com>
2025-03-16 13:37:09 +0500

    tray + removed useless stuff

--- main.js
+++ main.js
@@ -1,30 +1,52 @@
-const { app, BrowserWindow } = require('electron/main')
-const path = require('node:path')
+const { app, BrowserWindow, Menu, Tray } = require('electron');
+const path = require('node:path');
 
-function createWindow () {
-  const win = new BrowserWindow({
+let tray = null;
+let win = null;
+
+function createWindow() {
+  win = new BrowserWindow({
     width: 800,
     height: 600,
+    autoHideMenuBar: true, // Hides the menu bar
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
     }
-  })
+  });
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
+
+  // Hide window instead of closing
+  win.on('close', (event) => {
+    event.preventDefault();
+    win.hide();
+  });
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
+
+  // Create system tray icon
+  tray = new Tray(path.join(__dirname, 'icon.png')); // Change to your icon path
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Show', click: () => win.show() },
+    { label: 'Quit', click: () => { app.quit(); } }
+  ]);
+
+  tray.setToolTip('Flowtime Timer');
+  tray.setContextMenu(contextMenu);
+
+  tray.on('click', () => win.show()); // Show window on tray icon click
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
